484824a6ad66d0a24b0bd51c918741ddb04a9a02
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-28 22:15:43 -0500

    Update and rename twoSumTwo to twoSumTwo.py

---
+++ twoSumTwo.py
@@ -0,0 +1,9 @@
+#167. Two Sum II - Input Array Is Sorted
+
+class Solution(object):
+    def twoSumTwo(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
